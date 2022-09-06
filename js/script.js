20bb7704c9849b96e3b4a83c375b92559d3973f5
Sourabh Bundele <sourabhbundele@gmail.com>
2022-09-06 17:32:03 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const initialPrice = document.querySelector("#initial-price");
+const quantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submit-btn");
+const output = document.querySelector("#output-box");
+
